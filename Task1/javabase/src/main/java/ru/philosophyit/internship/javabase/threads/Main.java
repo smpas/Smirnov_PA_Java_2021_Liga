0dea90111db77745698bb5d919f400930a60e126
smpas <smpashts@gmail.com>
2021-09-23 00:00:41 +0300

    Add answers about threads

--- Task1/javabase/src/main/java/ru/philosophyit/internship/javabase/threads/Main.java
+++ Task1/javabase/src/main/java/ru/philosophyit/internship/javabase/threads/Main.java
@@ -6,9 +6,22 @@ import java.util.concurrent.Executors;
 public class Main {
 
     // Почему при вызове executorService.shutdown(); программа продолжает свое исполнение ?
+    /*
+    Потому что метод shutdown() завершает работу пула потоков только после завершения
+    уже принятых на обработку задач. Для неме
+     */
+
     // Почему если убрать строчку 28 (executorService.shutdown()) программа не прекратит свое исполнение
     // даже после завершения всех тасок в executorService ?
+    /*
+    Потому что потоки в пуле потоков не завершают свое выполнение самостоятельно, даже при обработке всех задач.
+    Поэтому программа не завершает свое выполнение, т.к. остались активные потоки пула.
+     */
+
     // Почему при работе тасок executorService в консоль в секунду попадает всего 4 сообщения, тогда как тасок в executorService - 16?
+    /*
+    Потому что пул потоков инициализирован четырьмя потоками, которые могут выполнять только 4 задачи в один момент времени.
+     */
     public static void main(String[] args) {
         startSomeDaemon();
 
@@ -27,7 +40,7 @@ public class Main {
                 System.err.println(String.format("Hello from %d callable", captureId));
             });
         }
-//        executorService.shutdown();
+        executorService.shutdown();
     }
 
     private static int getThreadsCount() {
