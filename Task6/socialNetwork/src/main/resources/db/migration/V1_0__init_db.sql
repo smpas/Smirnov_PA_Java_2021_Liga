d16252d9e680b6e59188d94a08a9c647d626cd2f
smpas <smpashts@gmail.com>
2021-09-30 01:53:00 +0300

    Add sixth homework

---
+++ Task6/socialNetwork/src/main/resources/db/migration/V1_0__init_db.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS School
+(
+    Id SERIAL PRIMARY KEY,
+    Name CHAR(30),
+    Address CHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS Client
+(
+    Id SERIAL PRIMARY KEY,
+    Name CHAR(30),
+    Nickname CHAR(20),
+    Sex CHAR(1),
+    SchoolId INTEGER,
+    FOREIGN KEY (SchoolId) REFERENCES School(Id) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS Friend
+(
+    FirstUser INTEGER,
+    SecondUser INTEGER,
+    FOREIGN KEY (FirstUser) REFERENCES Client(Id) ON DELETE CASCADE,
+    FOREIGN KEY (SecondUser) REFERENCES Client(Id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Post
+(
+    Id SERIAL PRIMARY KEY,
+    ClientId INTEGER,
+    Date TIMESTAMP,
+    Text CHAR(1024),
+    FOREIGN KEY (ClientId) REFERENCES Client(Id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS PrivateMessages
+(
+    Id SERIAL PRIMARY KEY,
+    SenderId INTEGER,
+    RecipientId INTEGER,
+    Date TIMESTAMP,
+    Text CHAR(1024),
+    FOREIGN KEY (SenderId) REFERENCES Client(Id) ON DELETE SET NULL,
+    FOREIGN KEY (RecipientId) REFERENCES Client(Id) ON DELETE SET NULL
+)
