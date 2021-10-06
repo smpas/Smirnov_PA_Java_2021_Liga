30cff045f0a97de262c36846a0be1bbe837f087d
smpas <smpashts@gmail.com>
2021-10-06 17:14:53 +0300

    Change naming and add many-to-many relationship

--- Task6/socialNetwork/src/main/resources/db/migration/V1_0__init_db.sql
+++ Task6/socialNetwork/src/main/resources/db/migration/V1_0__init_db.sql
@@ -1,45 +1,62 @@
-CREATE TABLE IF NOT EXISTS School
+CREATE TABLE IF NOT EXISTS school
 (
-    Id SERIAL PRIMARY KEY,
-    Name CHAR(30),
-    Address CHAR(50)
+    id SERIAL PRIMARY KEY,
+    name CHAR(30),
+    address CHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS Client
+CREATE TABLE IF NOT EXISTS client
 (
-    Id SERIAL PRIMARY KEY,
-    Name CHAR(30),
-    Nickname CHAR(20),
-    Sex CHAR(1),
-    SchoolId INTEGER,
-    FOREIGN KEY (SchoolId) REFERENCES School(Id) ON DELETE SET NULL
+    id SERIAL PRIMARY KEY,
+    name CHAR(30),
+    surname CHAR(30),
+    nickname CHAR(30),
+    sex CHAR(1),
+    school_id INTEGER,
+    FOREIGN KEY (school_id) REFERENCES school(id) ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS Friend
+CREATE TABLE IF NOT EXISTS friend
 (
-    FirstClient INTEGER,
-    SecondCLient INTEGER,
-    PRIMARY KEY (FirstClient, SecondCLient),
-    FOREIGN KEY (FirstClient) REFERENCES Client(Id) ON DELETE CASCADE,
-    FOREIGN KEY (SecondCLient) REFERENCES Client(Id) ON DELETE CASCADE
+    first_client INTEGER,
+    second_cLient INTEGER,
+    PRIMARY KEY (first_client, second_cLient),
+    FOREIGN KEY (first_client) REFERENCES client(id) ON DELETE CASCADE,
+    FOREIGN KEY (second_cLient) REFERENCES client(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS Post
+CREATE TABLE IF NOT EXISTS post
 (
-    Id SERIAL PRIMARY KEY,
-    ClientId INTEGER,
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER,
+    date TIMESTAMP,
+    header CHAR(50),
+    text CHAR(300),
+    FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS dialog
+(
+    id SERIAL PRIMARY KEY,
+    name CHAR(30)
+);
+
+CREATE TABLE IF NOT EXISTS client_dialog
+(
+    client_id INTEGER,
+    dialog_id INTEGER,
+    PRIMARY KEY (client_id, dialog_id),
+    FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE CASCADE,
+    FOREIGN KEY (dialog_id) REFERENCES dialog(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS message
+(
+    id SERIAL PRIMARY KEY,
+    client_id INTEGER,
+    dialog_id INTEGER,
     Date TIMESTAMP,
-    Text CHAR(1024),
-    FOREIGN KEY (ClientId) REFERENCES Client(Id) ON DELETE CASCADE
+    Text CHAR(300),
+    FOREIGN KEY (client_id) REFERENCES client(id) ON DELETE SET NULL,
+    FOREIGN KEY (dialog_id) REFERENCES dialog(Id) ON DELETE CASCADE
 );
-
-CREATE TABLE IF NOT EXISTS PrivateMessages
-(
-    Id SERIAL PRIMARY KEY,
-    SenderId INTEGER,
-    RecipientId INTEGER,
-    Date TIMESTAMP,
-    Text CHAR(1024),
-    FOREIGN KEY (SenderId) REFERENCES Client(Id) ON DELETE SET NULL,
-    FOREIGN KEY (RecipientId) REFERENCES Client(Id) ON DELETE SET NULL
-)
